d220335447d58cd04e8502d358169b28826ea81e
sazzad <hossensazzad1201@gmail.com>
2022-05-07 00:12:42 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Shared/Header/Header';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+      <Footer></Footer>
 
     </div>
   );
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Items from '../Items/Items';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is home</h2>
+            <Items></Items>
+        </div>
+    );
+};
+
+export default Home;
